fe99555f08ea46267ad26f26a495cae6cbdbf535
Saoud Nazir <31433177+saoudnazir@users.noreply.github.com>
2024-11-14 20:11:48 +0800

    Delete src/modules/agents directory

--- src/modules/agents/agents.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AgentsService } from './agents.service';
-import { AgentsController } from './agents.controller';
-
-@Module({
-  controllers: [AgentsController],
-  providers: [AgentsService],
-})
-export class AgentsModule {}
